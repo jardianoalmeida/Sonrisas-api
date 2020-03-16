09eaa34116f490c2fc431057af3d74e5e0c38b6f
Jardiano Almeida <jardiano.almeida@gmail.com>
2020-03-16 13:35:42 -0300

    first commit

---
+++ src/main/java/com/jardiano/sonrisas/SonrisasApplication.java
@@ -0,0 +1,13 @@
+package com.jardiano.sonrisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonrisasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonrisasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jardiano/sonrisas/SonrisasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jardiano.sonrisas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonrisasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
